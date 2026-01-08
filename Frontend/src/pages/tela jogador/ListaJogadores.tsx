68ccbffa09435f2d165f41af308a23f87a0d922d
pedrohenry12 <faquinipedro12@gmail.com>
2026-01-08 12:02:21 -0300

    Update ListaJogadores.tsx

--- Frontend/src/pages/tela jogador/ListaJogadores.tsx
+++ Frontend/src/pages/tela jogador/ListaJogadores.tsx
@@ -1,4 +1,4 @@
-import styles from "./listaJogadores.module.css";
+import styles from "./listajogadores.module.css";
 import { deleteJogador } from "../../services/jogadoresServices";
 
 interface Jogador {
